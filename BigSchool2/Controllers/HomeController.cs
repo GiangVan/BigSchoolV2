8b28db0bb2c245eb366e965d4cacf276baa8e706
GiangVan <noname7652259@gmail.com>
2019-04-05 01:57:05 +0700

    go to the bed

--- BigSchool2/Controllers/HomeController.cs
+++ BigSchool2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BigSchool2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,21 @@ namespace BigSchool2.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
